2df0e003559389aa15b6041676936aa8ffa48937
George Miller <git@duffleman.co.uk>
2020-04-01 01:30:51 +0100

    purchased a new domain!

--- dnsconfig.js
+++ dnsconfig.js
@@ -102,6 +102,23 @@ D("duffleman.dev", REG_NONE, DnsProvider(cloudflare),
 	TXT('google._domainkey', 'v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsc0Yj+RLfT+MirS/ZpLyRxXpHfdxZYRGbK5zS+X4ASGVIIZLA9EdK7qkNnNDxb6ID6wdx9Z52H/DRsOwhG1g21MA2SVL+43d41gdbAjAussF1iA6T/UD7oGWutSS9kW3RvFYoYm9NEr9dJmGHj2WiOEunruw+O0gDVyXrdWI2idsx2sTU61yE+RUdPJ/uAMg3Q5rWkWzqW22+eFFgE8MG6we7qPzEbbzg23mDy15UJnqE7kxIrgsfgYbsHb4H2NE6BvKKpqoHJ40W1ozmLaX0EoiCSExzoeb+lsPp/nSiH8ePy65nizzVioNf16sI39Z6v6zn87XVaPri6v5pMDh8QIDAQAB')
 )
 
+D("duffleman.me", REG_NONE, DnsProvider(cloudflare),
+	A('@', '1.2.3.4', CF_PROXY_ON),
+	A('www', '1.2.3.4', CF_PROXY_ON),
+
+	CF_REDIRECT("*duffleman.me/*", "https://duffleman.co.uk/$2"),
+
+	MX('@', 1, 'aspmx.l.google.com.'),
+	MX('@', 5, 'alt1.aspmx.l.google.com.'),
+	MX('@', 5, 'alt2.aspmx.l.google.com.'),
+	MX('@', 10, 'aspmx2.googlemail.com.'),
+	MX('@', 10, 'aspmx3.googlemail.com.'),
+
+	TXT('@', 'v=spf1 include:_spf.google.com ~all'),
+	TXT('@', 'keybase-site-verification=krT8JY9BETiWK8m-QISEHETCJ49WetjrXXDTDHx_sEM'),
+	TXT('google._domainkey', 'v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsUTL7ILoIQfPJs0w24VA7d3LZBhZ8CoCA1xaQPctFgrDzP/gg1yaTBoH6m9K28OBBeAqRL2hw12AfV358woMQo+aMkOT5ghzNyjdMOoX55uYr/zN9jb30jL5BGf6zNFHK0kd1DLUonI0f+YdJW8bg0JWt2ftjZoMyL5fni/wnGFA3NtJ9vr/wdhQI+cg9FwPTInTWlAnwauzIqiqzSwSxH+XuLtTbv2uFZ+btcSru5QrCm6KHIMlWCOxgMaKgvkzCQPioiryOyAG8kmIDip0LM+Ci+meVbTHpbUBv3IU+4CfcEF3uCvAB1/4qjQm17SxM6y/lt/+pp2Ups15rh1krQIDAQAB')
+)
+
 D("duffleman.uk", REG_NONE, DnsProvider(cloudflare),
 	A('@', '1.2.3.4', CF_PROXY_ON),
 	A('www', '1.2.3.4', CF_PROXY_ON),
