9ef203691f42d53924e8dfe308039fc73a342624
George Miller <git@duffleman.co.uk>
2020-04-23 12:37:59 +0100

    split out provision script

--- k8s-support/provision/base.sh
+++ k8s-support/provision/base.sh
@@ -22,37 +22,6 @@ apt-transport-https \
 ca-certificates \
 curl \
 gnupg-agent \
-software-properties-common \
-docker-ce \
-docker-ce-cli \
-containerd.io \
-kubelet \
-kubeadm \
-kubectl
-
-modprobe br_netfilter
-
-cat <<EOF > /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sysctl --system
-
-cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-
-mkdir -p /etc/systemd/system/docker.service.d
-
-# Restart docker.
-systemctl daemon-reload
-systemctl restart docker
+software-properties-common
 
 apt autoremove -y
---
+++ k8s-support/provision/haproxy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+add-apt-repository ppa:vbernat/haproxy-1.8
+apt update
+apt install haproxy
